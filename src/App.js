1b9eb007233e9b98935a1cc4288413f5ebf65ba0
pritoo <preetmangal@gmail.com>
2022-03-16 16:37:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,74 @@
 
+
 import './App.css';
-import React, { useState, useMemo } from 'react'
+import React, { useState } from 'react'
 import Select from 'react-select'
-import countryList from 'react-select-country-list'
+//import countryList from 'react-select-country-list'
 import { Country, State, City }  from 'country-state-city';
 
 
 function App() {
-  const [value, setValue] = useState('')
-const [state_of_countary, setstate_of_countary] = useState('')
-  const [State_name, setstate_name] = useState('')
-  const options = useMemo(() => countryList().getData(), [])
+ const [value, setValue] = useState('')
+//const [state_of_countary, setstate_of_countary] = useState('')
+  //const [State_name, setstate_name] = useState('')
+  //const options = useMemo(() => countryList().getData(), [])
  // const State = useMemo(() => stateList().getData(), [])
 
-  const changeHandler = value => {
-    console.log(value);
-    Country.getAllCountries()
-     setstate_of_countary(State.getStatesOfCountry(value.value));
-    setValue(value)
-    setstate_name(State_name)
-  }
+//  const opt = languages.map(item => ({'value':item}));
+//  console.log(opt);
+const country = Country.getAllCountries()
+const opt =country.map(({isoCode:value, name:label}) => ({value,label}));
+console.log(opt); 
+ const state = State.getStatesOfCountry("IN")
  
+ //const res = state.map((state_name => ({'value':state_name,'label':state_name})));
+ const res = state.map(({isoCode:value, name:label}) => ({value,label}));
+ console.log(res); 
+ const city = City.getCitiesOfState("IN" ,"RJ")
+ //const citycode=[{"countryCode":"value"}, {"isoCode":"value"}, {"name":"value"}]
+ const rese = city.map(({isoCode:value, name:label}) => ({value,label}));
+ console.log(rese); 
+
+
+  const changeHandler = value => {
+   console.log(value); 
+   Country.getAllCountries()
+   State.getStatesOfCountry()
+   City.getCitiesOfState()
+
+    //setstate_of_countary(State.getStatesOfCountry(value.value));
+    setValue(value)
+    //setstate_name(State_name)
+  }
   
-  console.log(state_of_countary);
+ // console.log(Country.getAllCountries());
+  //console.log( State.getStatesOfCountry("IN"));
+  //console.log(city.getCitiesOfState("RJ"));
+  
   return (
-    <div className="App">
-     
-     <h1>Hello World</h1>
-     <div className='container'>
-     <Select options={options} value={value} onChange={(e)=>{changeHandler(e)}} />
-     <div className='mb-3 mx-auto'>
-     <Select options={state_of_countary} value={State_name} onChange={(e)=>{changeHandler(e)}} />
-     </div>
-     </div>
-    </div>
+
+    <>
+      <div className="App">
+      
+          <h1>Hello World</h1>
+          <div className='container'>
+            <div className='mb-3 mx-auto'>
+            
+              <Select options={country} value={value} onChange={(e)=>{changeHandler(e)}} placeholder="Country" />
+            </div>
+            <div className='mb-3 mx-auto'>
+            
+            <Select options={state} value={res} onChange={(e)=>{changeHandler(e)}}  placeholder="State"/>
+            </div>
+            <div className='mb-3 mx-auto'>
+          
+            <Select options={city} value={rese} onChange={(e)=>{changeHandler(e)}} placeholder="City" />
+            </div>
+          </div>
+      </div>
+    </>
   );
 }
 
 export default App;
+
